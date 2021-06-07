5376943175925a2b7f916d2d33b34e7386149508
LajjaYousef <Lajjayousef66@gmail.com>
2021-06-07 04:10:53 +0200

    Create Function Which i will Need it in Interface.

--- DemoCore.BLL/Interfaces/IDepartmentRep.cs
+++ DemoCore.BLL/Interfaces/IDepartmentRep.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DemoCore.BLL.Models.ViewModels;
+using DemoCore.DAL.Entity;
 
 namespace DemoCore.BLL.Interfaces
 {
  public interface IDepartmentRep
  {
      IEnumerable<DepartmentVM> Get();
+     DepartmentVM GetById (int id);
+     IEnumerable<DepartmentVM> SearchByName(string Name);  
+     void Create (DepartmentVM Obj);
+     void Edit (DepartmentVM obj); 
+     void Delete(int id);
+    
+
+
  }
 }
