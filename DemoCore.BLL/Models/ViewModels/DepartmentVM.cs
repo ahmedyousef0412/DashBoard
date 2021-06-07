45fc41b73bc35c65ba8889cf3b0528712922a148
LajjaYousef <Lajjayousef66@gmail.com>
2021-06-07 06:51:59 +0200

    Using Validation.
    Using Validation Client  and Server Side
    Jquery
    and Jquery Validation
    and Jquery Validation Unobtersive.
    put (Jquery Validation
    and Jquery Validation Unobtersive.) in Render Section in Main Layout under jquery libarary and go to my View using My Section.

--- DemoCore.BLL/Models/ViewModels/DepartmentVM.cs
+++ DemoCore.BLL/Models/ViewModels/DepartmentVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,14 @@ namespace DemoCore.BLL.Models.ViewModels
     {
         
         public int Id { get; set; }
+
+        [Required (ErrorMessage = "Enter Department Name")]
+        [MaxLength(50 , ErrorMessage = "Max Length 50")]
+        [MinLength(3 , ErrorMessage = "Min Length 3")]
         public string DepartmentName { get; set; }
+
+
+        [Required(ErrorMessage = "Enter Department Code")]
         public string Departmentcode { get; set; }
     }
 }
